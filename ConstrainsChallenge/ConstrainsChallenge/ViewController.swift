207b0451e64f7971a759092245a15d6a49235dc4
Emanuel Queiroz <em_pstxd@msn.com>
2018-05-23 13:23:53 -0300

    Full Files

---
+++ ConstrainsChallenge/ConstrainsChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConstrainsChallenge
+//
+//  Created by Emanuel Queiroz on 26/04/18.
+//  Copyright © 2018 Emanuel Queiroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
